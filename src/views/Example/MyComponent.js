a767d2f01e848d2022d87b3360730de8195342c7
KietHo1 <kiethostartup1999@gmail.com>
2023-03-12 20:44:21 +0700

    video 5 React season 1

--- src/views/Example/MyComponent.js
+++ src/views/Example/MyComponent.js
@@ -1,8 +1,36 @@
 import React from "react";
 
 class MyComponent extends React.Component {
+  //key:value
+  state = {
+    name: "Ronaldo",
+    channel: "Hoi Dan IT",
+  };
+
+  /* 
+    JSX => return block
+    fragment
+    */
+
+  handleOnChangeName = (event) => {
+    this.setState({
+      name: event.target.value,
+    });
+  };
   render() {
-    return <div>hello my component Hoi Dan IT</div>;
+    return (
+      <>
+        <div className="first">
+          <input
+            value={this.state.name}
+            type="text"
+            onChange={(event) => this.handleOnChangeName(event)}
+          />
+          My name is {this.state["name"]}
+        </div>
+        <div className="second">My youtube channel : {this.state.channel}</div>
+      </>
+    );
   }
 }
 
